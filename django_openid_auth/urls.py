8ba2ece2f3b89c8d31407501da684b407bf826ed
Stepan Henek <stepan.henek@nic.cz>
2013-06-06 15:36:19 +0200

    Making compatible with django 1.5

--- django_openid_auth/urls.py
+++ django_openid_auth/urls.py
@@ -29,8 +29,10 @@
 
 from django.conf.urls.defaults import *
 
+import views
+
 urlpatterns = patterns('django_openid_auth.views',
-    url(r'^login/$', 'login_begin', name='openid-login'),
-    url(r'^complete/$', 'login_complete', name='openid-complete'),
-    url(r'^logo.gif$', 'logo', name='openid-logo'),
+    url(r'^login/$', views.login_begin, name='openid-login'),
+    url(r'^complete/$', views.login_complete, name='openid-complete'),
+    url(r'^logo.gif$', views.logo, name='openid-logo'),
 )
