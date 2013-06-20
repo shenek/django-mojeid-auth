b5b872dab85d256fbd68fe3dca6e2b3005ae8573
Stepan Henek <stepan.henek@nic.cz>
2013-06-20 14:22:59 +0200

    Added signon to xrds.xml

--- django_mojeid/views.py
+++ django_mojeid/views.py
@@ -375,9 +375,11 @@ def top(request, template_name='openid/top.html'):
 def xrds(request, template_name='openid/xrds.xml'):
     return_to_url = request.build_absolute_uri(reverse(login_complete))
     assertion_url = request.build_absolute_uri(reverse(assertion))
+    top_url = request.build_absolute_uri(reverse(top))
     return render_to_response(template_name,
                               {'return_to_url': return_to_url,
-                               'assertion_url': assertion_url},
+                               'assertion_url': assertion_url,
+                               'top_url': top_url},
                               context_instance=RequestContext(request),
                               content_type=YADIS_CONTENT_TYPE
                              )
